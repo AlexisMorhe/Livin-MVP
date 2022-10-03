2d7ea972416d18739ac00b5fb6e2913eaa9d6101
Alexis <alexis@morhe.mx>
2022-10-03 02:15:16 -0500

    Currency format added to inputs

--- components/Bienvenido/preaprobacion.js
+++ components/Bienvenido/preaprobacion.js
@@ -11,7 +11,7 @@ export default function Preaprobacion() {
       <div className='w-4/5 h-3/5 rounded-3xl bg-yellow-300 flex flex-col pl-20 justify-center'>
         <div className='flex flex-col justify-center w-1/2 h-full'>
           <h1 className='font-domaine-bold tracking-wide font-bold text-[3.25rem] my-4 leading-tight'>Tienes un crédito<br/>preaprobado por
-            <CurrencyFormat value={((ahorros / 36) + ingreso) * 40} displayType={"text"} thousandSeparator={true} prefix={' $ '} fixedDecimalScale={true} decimalSeparator={''} decimalScale={0}/>
+            <CurrencyFormat value={ingreso * 40} displayType={"text"} thousandSeparator={true} prefix={' $ '} fixedDecimalScale={true} decimalSeparator={''} decimalScale={0}/>
           </h1>
           <p className='font-rebond my-4 text-xl'>Livin determina los montos basado en la información que proporcionas. Para poder otorgarte este crédito necesitamos todavía algo más de información.</p>
           <Link href='/bienvenido'><a className='mt-10 w-fit py-3 px-9 text-white bg-dark font-chillax font-medium rounded-md hover:bg-slate-700 transition-all'>Proximamente podrás continuar con el proceso...</a></Link>
