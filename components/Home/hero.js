0190ed0fa315b4b3d68d4bfde1e654dffa6277f6
AlexisMorhe <107775090+AlexisMorhe@users.noreply.github.com>
2022-10-07 12:43:16 -0500

    Update hero.js

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -9,8 +9,8 @@ export default function Hero() {
       <div className='flex flex-col h-full z-10 w-2/5 pl-20 justify-center relative'>
         <h1 className='font-domaine-bold tracking-wide text-[3.25rem] my-4 leading-tight w-5/6'>Consigue la casa <br/>de tus sueños sin enganche.</h1>
         <p className='font-rebond w-full my-4 text-xl'>Encuentra la casa de tus sueños, mudate ahora y rentala. Parte de renta se irá directo al enganche. Nuestro programa está diseñado para que puedas conseguir tu hipoteca en 3 años.</p>
-        <div className='flex justify-between w-full'>
-          <Link href='/registro'><a className='my-4 w-fit py-3 px-9 bg-yellow-300 font-chillax font-medium rounded-md hover:bg-yellow-200 transition-all'>Consigue tu casa</a></Link>
+        <div className='flex w-full'>
+          <Link href='/registro'><a className='my-4 mr-12 w-fit py-3 px-9 bg-yellow-300 font-chillax font-medium rounded-md hover:bg-yellow-200 transition-all'>Consigue tu casa</a></Link>
           <Link href='/vendetucasa'><a className='my-4 w-fit py-3 px-9 text-white bg-dark font-chillax font-medium rounded-md hover:bg-slate-700 transition-all'>Vende tu casa</a></Link>
         </div>
       </div>
